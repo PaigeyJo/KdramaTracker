dc7d0d2d7121cf525066d7c3a7a87b08ac9176c5
Paige Johanson <paigejohanson9@gmail.com>
2023-05-04 17:39:59 -0400

    created kdrama and user kdrama pages

---
+++ client/src/pages/Kdramas.js
@@ -0,0 +1,9 @@
+function Kdramas () {
+    return (
+        <div>
+            <h1>Kdramas</h1>
+        </div>        
+    )
+}
+
+export default Kdramas;
---
+++ client/src/pages/UserKdramas.js
@@ -0,0 +1,9 @@
+function UserKdramas () {
+    return (
+        <div>
+            <h1>UserKdramas</h1>
+        </div>
+    )
+}
+
+export default UserKdramas;
