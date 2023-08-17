f2d21cd0e2d14cbfefc4e1d28d95d75730feebe6
Paige Johanson <paigejohanson9@gmail.com>
2023-08-16 20:34:57 -0400

    populate getone action

--- api/src/controllers/kdramas/getOne.action.ts
+++ api/src/controllers/kdramas/getOne.action.ts
@@ -3,7 +3,34 @@ import { KDrama } from '@@entities/index.js';
 
 import { entities, errors } from '@@utils/index.js';
 
-function getOne(req: Request, res: Response, next: NextFunction) {
+interface Params {
+    id?: number
+};
+
+ async function getOne(req: Request<any, Params>, res: Response, next: NextFunction) {
+    const { id } = req.params;
+
+    if(!id){
+        return errors.sendInvalidBody(res);
+    }
+
+    const [kDrama, findErr] = await entities.findOne<KDrama>(KDrama, {
+        where: {
+            id: id
+        }
+    });
+
+    if(findErr || !kDrama) {
+        return errors.sendEntitiesResponse({
+            res,
+            err: findErr,
+            message: "Unable to find Kdrama",
+            entityReturn: kDrama,
+            missingEntityReturnMessage: "No Kdrama Returned"
+        });
+    }
+    
+    return res.json({ results: kDrama });
 
 };
 
