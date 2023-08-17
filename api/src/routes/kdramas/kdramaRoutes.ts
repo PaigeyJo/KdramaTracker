54ebb550166372a853c6f6fd87e2cca60871f963
Paige Johanson <paigejohanson9@gmail.com>
2023-08-16 20:59:12 -0400

    index for kdrama

--- api/src/routes/kdramas/kdramaRoutes.ts
+++ api/src/routes/kdramas/kdramaRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import * as kdramasController from '@@controllers/kdramas/index.js';
+import { extractPaginationParams } from '@@middleware/index.js';
 
 const router = express.Router();
 
 router.route("/")
-.get(kdramasController.index)
+.get(extractPaginationParams, kdramasController.index)
 .post(kdramasController.create)
 
 router.route("/id/:id")
