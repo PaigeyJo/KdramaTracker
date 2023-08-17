df6e3435bb595f11ad0eee0e39fd64f9aa0c01c6
Paige Johanson <paigejohanson9@gmail.com>
2023-08-16 20:59:27 -0400

    index for kdrama

--- api/src/controllers/kdramas/index.action.ts
+++ api/src/controllers/kdramas/index.action.ts
@@ -1,10 +1,28 @@
-import { Request, Response, NextFunction } from '@@types/express.js';
+import { Request, NextFunction, PaginatedResponse } from '@@types/express.js';
+
 import { KDrama } from '@@entities/index.js';
 
-import { entities, errors } from '@@utils/index.js';
+import { entities, errors, pagination } from '@@utils/index.js';
 
-function index(req: Request, res: Response, next: NextFunction) {
-    return res.json({ message: "This route no work yet" });
+async function index(req: Request, res: PaginatedResponse, next: NextFunction) {
+
+    const { limit, offset } = res.locals.pagination;
+
+    const [kDramas, err] = await entities.indexAndCount<KDrama>(KDrama, {
+        limit, offset
+    });
+
+    if(err || !kDramas) {
+        return errors.sendEntitiesResponse({
+            res,
+            err,
+            message: "Unable to find Kdrama",
+            entityReturn: kDramas,
+            missingEntityReturnMessage: "No Kdrama Returned"
+        });
+    }
+    const response = pagination.paginateResponse<KDrama>(req, res, kDramas);
+    return res.json(response);
 };
 
 export default index;
